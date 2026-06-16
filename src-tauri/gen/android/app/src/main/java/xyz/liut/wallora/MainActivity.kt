6986ebee836e3962d74d3b1f359a7e5c9a567e57
liutong <liut@koal.com>
2026-06-16 19:54:50 +0800

    fix: android ui

--- src-tauri/gen/android/app/src/main/java/xyz/liut/wallora/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/xyz/liut/wallora/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
-    super.onCreate(savedInstanceState)
-  }
+    override val handleBackNavigation: Boolean = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+    }
 }
